fa66be30fb0487408c9267ebee5918f014531064
bellaaa001 <25moph01@uohyd.ac.in>
2026-06-05 20:29:38 +0530

    Add configuration module

---
+++ js/config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+  PAGES: [
+    "setup",
+    "consent",
+    "aft",
+    "practice",
+    "scientific-color",
+    "face-color",
+    "color-face",
+    "memory",
+    "summary"
+  ],
+
+  EVENT_TYPES: {
+    PAGE_ENTER: "page_enter",
+    PAGE_EXIT: "page_exit",
+    SESSION_START: "session_start",
+    SESSION_END: "session_end",
+    RESPONSE: "response",
+    SKIP: "skip",
+    PAUSE: "pause",
+    RESUME: "resume",
+    STOP: "stop"
+  }
+};
